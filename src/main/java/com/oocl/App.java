134cf38eeee095e29717426955d1a0cace448d34
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-08 19:25:31 +0800

    completed input handler

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -1,13 +1,25 @@
 package com.oocl;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static List<Integer> getNext(){
+        String input = scanner.nextLine();
+        return Arrays.stream(input.split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        while(true){
+            System.out.println(getNext());
+        }
     }
 }
