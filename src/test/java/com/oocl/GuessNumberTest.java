37199cbfc8b80d9a67387fddc95d1eacbfda718a
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-09 11:51:44 +0800

    replace getFeedBack test cse by general equiuvalent test

--- src/test/java/com/oocl/GuessNumberTest.java
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -44,17 +44,44 @@ public class GuessNumberTest {
     }
 
     @Test
-    public void should_count_3_correct_number() {
-        List<Integer> input = Arrays.asList(2, 1, 5, 3);
+    public void should_return_4A0B_when_input_1234_given_answer_1234() {
+        List<Integer> input = Arrays.asList(1, 2, 3, 4);
         String result = guessNumber.getFeedback(input);
-        assertEquals("0A3B", result);
+        assertEquals("4A0B", result);
     }
 
     @Test
-    public void should_count_1_correct_position_and_1_correct_number() {
-        List<Integer> input = Arrays.asList(9, 2, 5, 3);
-        List<Integer> answer = Arrays.asList(1, 2, 3, 4);
+    public void should_return_0A4B_when_input_4321_given_answer_1234() {
+        List<Integer> input = Arrays.asList(4, 3, 2, 1);
         String result = guessNumber.getFeedback(input);
-        assertEquals("1A1B", result);
+        assertEquals("0A4B", result);
+    }
+
+    @Test
+    public void should_return_0A0B_when_input_5678_given_answer_1234() {
+        List<Integer> input = Arrays.asList(5, 6, 7, 8);
+        String result = guessNumber.getFeedback(input);
+        assertEquals("0A0B", result);
+    }
+
+    @Test
+    public void should_return_2A0B_when_input_1256_given_answer_1234() {
+        List<Integer> input = Arrays.asList(1, 2, 5, 6);
+        String result = guessNumber.getFeedback(input);
+        assertEquals("2A0B", result);
+    }
+
+    @Test
+    public void should_return_0A2B_when_input_1256_given_answer_1234() {
+        List<Integer> input = Arrays.asList(2, 1, 5, 6);
+        String result = guessNumber.getFeedback(input);
+        assertEquals("0A2B", result);
+    }
+
+    @Test
+    public void should_return_2A2B_when_input_1243_given_answer_1234() {
+        List<Integer> input = Arrays.asList(1, 2, 4, 3);
+        String result = guessNumber.getFeedback(input);
+        assertEquals("2A2B", result);
     }
 }
